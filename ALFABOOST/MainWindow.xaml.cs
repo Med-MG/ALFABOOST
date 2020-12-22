3c4d56e17d0d02aeb304c7560fc201acc37df4c8
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-12-22 11:57:26 +0100

    Add project files.

---
+++ ALFABOOST/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ALFABOOST
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Window(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+        private void Minimize_Window(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void max_window(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void Drage_appRight(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+    }
+}
