04bfd4ce23b050b875842eea287646ff8f67d504
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-12-23 16:53:46 +0100

    Scan method

---
+++ ALFABOOST/Analyzer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ALFABOOST
+{
+    class Analyzer 
+    {
+        private static long FullSize = 0;
+
+        public IDictionary<string, long> Analyze()
+        {
+
+            IDictionary<string, long> DirTypesSizes = new Dictionary<string, long>();
+            //var MyDiroctory = @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del";
+
+            //Dictionary Of Diroctory Types,
+
+            /*
+             * A Dictionary Containing the type of Directories that we are going to analyze 
+             * Type : Path
+             * 
+             */
+            var DirTypesPaths = new Dictionary<string, string>()
+            {
+                {"Windows", @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del\windowsTemp" },
+                {"Browsers", @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del\Browsers" },
+                {"Software", @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del\Software" },
+                {"Multimedia", @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del\Multimedia" },
+            };
+
+            
+
+            try
+            {
+
+                foreach(var DirPaths in DirTypesPaths)
+                {
+                    DirTypesSizes.Add(DirPaths.Key, GetSize(DirPaths.Value));
+                }
+
+                DirTypesSizes.Add("FullSize", FullSize);
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+            return DirTypesSizes;
+        }
+
+        public static long GetSize(string path)
+        {
+            long currentSize = 0;
+            var dirInfo = new DirectoryInfo(path);
+            foreach (FileInfo fi in dirInfo.GetFiles("*", SearchOption.AllDirectories))
+            {
+                FullSize += fi.Length;
+                currentSize += fi.Length;
+                
+                //Display Files paths in UI
+
+            }
+
+            return currentSize;
+        }
+    }
+}
--- ALFABOOST/MainWindow.xaml.cs
+++ ALFABOOST/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +12,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,6 +28,10 @@ namespace ALFABOOST
         {
             InitializeComponent();
         }
+
+
+        private static long FullSize = 0;
+        IDictionary<string, long>  DirTypesSizes = new Dictionary<string, long>();
 
         private void Close_Window(object sender, MouseButtonEventArgs e)
         {
@@ -42,6 +50,116 @@ namespace ALFABOOST
         {
             this.DragMove();
         }
+
+        //private void StartScan(object sender, MouseButtonEventArgs e)
+        //{
+        //    Analyzer StartScan = new Analyzer();
+        //    ScanCircle.Value++;
+        //}
+
+
+        private void StartScan(object sender, RoutedEventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += Analyze;
+            worker.ProgressChanged += worker_ProgressChanged;
+
+            worker.RunWorkerAsync();
+        }
+
+        //void worker_DoWork(object sender, DoWorkEventArgs e)
+        //{
+        //    for (int i = 0; i < 100; i++)
+        //    {
+        //        (sender as BackgroundWorker).ReportProgress(i);
+        //        Thread.Sleep(100);
+        //    }
+        //}
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ScanCircle.Value = e.ProgressPercentage;
+            WaveProgress.Value = e.ProgressPercentage;
+        }
+
+
+        
+
+        void Analyze(object sender, DoWorkEventArgs e)
+        {
+
+            int count = 0;
+            //var MyDiroctory = @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del";
+
+            //Dictionary Of Diroctory Types,
+
+            /*
+             * A Dictionary Containing the type of Directories that we are going to analyze 
+             * Type : Path
+             * 
+             */
+            var DirTypesPaths = new Dictionary<string, string>()
+            {
+                {"Windows", @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del\windowsTemp" },
+                {"Browsers", @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del\Browsers" },
+                {"Software", @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del\Software" },
+                {"Multimedia", @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\WPF\FilesToClean\del\Multimedia" },
+            };
+
+
+
+            try
+            {
+
+                foreach (var DirPaths in DirTypesPaths)
+                {
+                    long currentSize = 0;
+                    
+                    var dirInfo = new DirectoryInfo(DirPaths.Value);
+                    foreach (FileInfo fi in dirInfo.GetFiles("*", SearchOption.AllDirectories))
+                    {
+                       
+                        FullSize += fi.Length;
+                        currentSize += fi.Length;
+
+                        //Display Files paths in UI
+                    }
+                    (sender as BackgroundWorker).ReportProgress(count+=25);
+                    Thread.Sleep(100);
+                    DirTypesSizes.Add(DirPaths.Key, currentSize);
+                }
+
+                DirTypesSizes.Add("FullSize", FullSize);
+
+                
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            HideDashboard();
+        }
+
+        void HideDashboard()
+        {
+
+            this.Dispatcher.Invoke(() =>
+            {
+                //Scan_Card.Visibility = Visibility.Hidden;
+
+                Storyboard sb = this.FindResource("Select_Cleaner") as Storyboard;
+                sb.Begin();
+            });
+
+           
+        }
+
+
+
 
     }
 }
